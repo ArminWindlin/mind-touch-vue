15f1bb0ded42c31b4469d566522a5597d9eb2c09
ArminWindlin <armin.windlin@gmail.com>
2019-11-23 14:36:39 +0100

    code reformatting

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -1,79 +1,82 @@
 <template>
-    <div class="game">
-        <view-canvas :character1="character1" ref="view"></view-canvas>
-    </div>
+  <div class="game">
+    <view-canvas :character1="character1" ref="view"></view-canvas>
+  </div>
 </template>
 
 <script>
-  import ViewCanvas from './View.vue';
+    import ViewCanvas from './View.vue';
 
-  export default {
-    name: 'game',
-    props: [],
-    components: {ViewCanvas},
-    data() {
-      return {
-        character1: {
-          x: 0,
-          y: 0,
-          exactX: 0,
-          exactY: 0,
+    export default {
+        name: 'game',
+        props: [],
+        components: {ViewCanvas},
+        data() {
+            return {
+                character1: {
+                    x: 0,
+                    y: 0,
+                    exactX: 0,
+                    exactY: 0,
+                },
+            };
         },
-      };
-    },
-    methods: {
-      processKeyPress(keyCode) {
-        let animationWidth = this.$refs.view.rectSize;
-        let counter = 0;
-        let move = {
-          left: false,
-          up: false,
-          right: false,
-          down: false,
-        };
-        // LEFT
-        if (keyCode === 37) {
-          move.left = true;
-        }
-        // UP
-        else if (keyCode === 38) {
-          move.up = true;
-        }
-        // RIGHT
-        else if (keyCode === 39) {
-          move.right = true;
-        }
-        // DOWN
-        else if (keyCode === 40) {
-          move.down = true;
-        }
+        methods: {
+            processKeyPress(keyCode) {
+                let animationWidth = this.$refs.view.rectSize;
+                let counter = 0;
+                let move = {
+                    left: false,
+                    up: false,
+                    right: false,
+                    down: false,
+                };
+                // LEFT
+                if (keyCode === 37) {
+                    move.left = true;
+                }
+                // UP
+                else if (keyCode === 38) {
+                    move.up = true;
+                }
+                // RIGHT
+                else if (keyCode === 39) {
+                    move.right = true;
+                }
+                // DOWN
+                else if (keyCode === 40) {
+                    move.down = true;
+                }
 
-        // move
-        this.character1.x++;
-        let speed1 = 2;
-        let interval = setInterval(() => {
-          if (move.right) this.character1.exactX += speed1;
-          if (move.left) this.character1.exactX -= speed1;
-          if (move.up) this.character1.exactY -= speed1;
-          if (move.down) this.character1.exactY += speed1;
-          counter += speed1;
-          if (counter >= animationWidth) clearInterval(interval);
-        }, 10);
-      },
-    },
-    beforeMount() {
-    },
-    mounted() {
-      // key listener
-      let self = this;
-      document.addEventListener('keydown', function(event) {
-        self.processKeyPress(event.keyCode);
-      });
-    },
-  };
+                // move
+                this.character1.x++;
+                let speed1 = 2;
+                let interval = setInterval(() => {
+                    if (move.right) this.character1.exactX += speed1;
+                    if (move.left) this.character1.exactX -= speed1;
+                    if (move.up) this.character1.exactY -= speed1;
+                    if (move.down) this.character1.exactY += speed1;
+                    counter += speed1;
+                    if (counter >= animationWidth) {
+                        clearInterval(interval);
+                    }
+                    ;
+                }, 10);
+            },
+        },
+        beforeMount() {
+        },
+        mounted() {
+            // key listener
+            let self = this;
+            document.addEventListener('keydown', function(event) {
+                self.processKeyPress(event.keyCode);
+            });
+        },
+    };
 </script>
 
 <style scoped>
-    @media screen and (max-width: 550px) {
-    }
+  @media screen and (max-width: 550px) {
+  }
 </style>
--- src/components/View.vue
+++ src/components/View.vue
@@ -1,109 +1,109 @@
 <template>
-    <canvas class="view-canvas" ref="canvas" :width="gameWidth" :height="gameHeight"
-            :style="'left:' + (innerWidth - gameWidth) / 2 + 'px; top:' + (innerHeight - gameHeight) / 2 + 'px'">
-    </canvas>
+  <canvas class="view-canvas" ref="canvas" :width="gameWidth" :height="gameHeight"
+          :style="'left:' + (innerWidth - gameWidth) / 2 + 'px; top:' + (innerHeight - gameHeight) / 2 + 'px'">
+  </canvas>
 </template>
 
 <script>
-  export default {
-    name: 'view-canvas',
-    props: ['character1'],
-    data() {
-      return {
-        gameWidth: 640,
-        gameHeight: 360,
-        innerWidth: 0,
-        innerHeight: 0,
-        ctx: null,
-        startTime: null,
-        fps: 0,
-        frameCount: 0,
-        animationTestNumber: 0,
-        rectSize: 0,
-      };
-    },
-    methods: {
-      // ration 16:9
-      render() {
-        let ctx = this.ctx;
-        ctx.save();
-        ctx.clearRect(0, 0, this.gameWidth, this.gameHeight);
+    export default {
+        name: 'view-canvas',
+        props: ['character1'],
+        data() {
+            return {
+                gameWidth: 640,
+                gameHeight: 360,
+                innerWidth: 0,
+                innerHeight: 0,
+                ctx: null,
+                startTime: null,
+                fps: 0,
+                frameCount: 0,
+                animationTestNumber: 0,
+                rectSize: 0,
+            };
+        },
+        methods: {
+            // ration 16:9
+            render() {
+                let ctx = this.ctx;
+                ctx.save();
+                ctx.clearRect(0, 0, this.gameWidth, this.gameHeight);
 
-        //ctx.strokeRect(0, 0, this.gameWidth, this.gameHeight);
+                //ctx.strokeRect(0, 0, this.gameWidth, this.gameHeight);
 
-        ctx.fillStyle = 'rgba(0, 0, 255, 1)';
-        //ctx.fillRect(10, 10, 100 + this.animationTestNumber, 100);
+                ctx.fillStyle = 'rgba(0, 0, 255, 1)';
+                //ctx.fillRect(10, 10, 100 + this.animationTestNumber, 100);
 
-        ctx.font = 'bold 18px Verdana';
-        ctx.textAlign = 'center';
+                ctx.font = 'bold 18px Verdana';
+                ctx.textAlign = 'center';
 
-        ctx.fillText(this.fps + ' FPS', this.gameWidth - 80, 20);
+                ctx.fillText(this.fps + ' FPS', this.gameWidth - 80, 20);
 
-        // get responsive rectangle size
-        this.rectSize = Math.floor(this.gameWidth / 14);
-        let rectSize = this.rectSize;
+                // get responsive rectangle size
+                this.rectSize = Math.floor(this.gameWidth / 14);
+                let rectSize = this.rectSize;
 
-        // draw character 1
-        ctx.fillStyle = 'rgb(0, 0, 255)';
-        ctx.fillRect(this.character1.exactX, this.character1.exactY, rectSize, rectSize);
+                // draw character 1
+                ctx.fillStyle = 'rgb(0, 0, 255)';
+                ctx.fillRect(this.character1.exactX, this.character1.exactY, rectSize, rectSize);
 
-        // grid
-        for (let i = 0; i < 14; i++) {
-          for (let j = 0; j < 7; j++) {
-            ctx.fillStyle = 'rgba(0, 0, 0, 0.8)';
-            ctx.strokeRect(i * rectSize, j * rectSize, rectSize, rectSize);
-          }
-        }
+                // grid
+                for (let i = 0; i < 14; i++) {
+                    for (let j = 0; j < 7; j++) {
+                        ctx.fillStyle = 'rgba(0, 0, 0, 0.8)';
+                        ctx.strokeRect(i * rectSize, j * rectSize, rectSize, rectSize);
+                    }
+                }
 
-        ctx.restore();
+                ctx.restore();
 
-        const sinceStart = Date.now() - this.startTime;
-        this.fps = Math.round(1000 / (sinceStart / ++this.frameCount) * 100) / 100;
-        window.requestAnimationFrame(this.render);
-      },
-      animationTest() {
-        let interval = setInterval(() => {
-          this.animationTestNumber++;
-          if (this.animationTestNumber > 150) clearInterval(interval);
-        }, 30);
-      },
-      setGameSize() {
-        if (window.innerHeight * 2 < window.innerWidth) {
-          this.gameHeight = window.innerHeight;
-          this.gameWidth = Math.round(this.gameHeight * 2);
-        } else {
-          this.gameWidth = window.innerWidth;
-          this.gameHeight = Math.round(this.gameWidth / 2);
-        }
-        this.innerWidth = window.innerWidth;
-        this.innerHeight = window.innerHeight;
-      },
-    },
-    beforeMount() {
-    },
-    mounted() {
-      const c = this.$refs.canvas;
-      this.ctx = c.getContext('2d');
-      this.startTime = Date.now();
-      window.requestAnimationFrame(this.render);
+                const sinceStart = Date.now() - this.startTime;
+                this.fps = Math.round(1000 / (sinceStart / ++this.frameCount) * 100) / 100;
+                window.requestAnimationFrame(this.render);
+            },
+            animationTest() {
+                let interval = setInterval(() => {
+                    this.animationTestNumber++;
+                    if (this.animationTestNumber > 150) clearInterval(interval);
+                }, 30);
+            },
+            setGameSize() {
+                if (window.innerHeight * 2 < window.innerWidth) {
+                    this.gameHeight = window.innerHeight;
+                    this.gameWidth = Math.round(this.gameHeight * 2);
+                } else {
+                    this.gameWidth = window.innerWidth;
+                    this.gameHeight = Math.round(this.gameWidth / 2);
+                }
+                this.innerWidth = window.innerWidth;
+                this.innerHeight = window.innerHeight;
+            },
+        },
+        beforeMount() {
+        },
+        mounted() {
+            const c = this.$refs.canvas;
+            this.ctx = c.getContext('2d');
+            this.startTime = Date.now();
+            window.requestAnimationFrame(this.render);
 
-      this.animationTest();
+            this.animationTest();
 
-      this.setGameSize();
-      window.addEventListener('resize', () => {
-        this.setGameSize();
-      });
+            this.setGameSize();
+            window.addEventListener('resize', () => {
+                this.setGameSize();
+            });
 
-    },
-  };
+        },
+    };
 </script>
 
 <style lang="scss" scoped>
-    .view-canvas {
-        position: absolute;
-        top: 0;
-    }
+  .view-canvas {
+    position: absolute;
+    top: 0;
+  }
 
-    @media screen and (max-width: 550px) {
-    }
+  @media screen and (max-width: 550px) {
+  }
 </style>

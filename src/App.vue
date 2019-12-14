ece06f8c4711681e99249845b1684645fe177f1a
ArminWindlin <armin.windlin@gmail.com>
2019-12-14 14:39:03 +0100

    added levels screen

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,23 @@
 <template>
   <div id="app">
-    <menuu v-if="menuC" @play="play()"></menuu>
+    <menuu v-if="menuC" @play="play()" @levels="levelSelectionC = true, menuC = false"></menuu>
+    <level-selection v-if="levelSelectionC" @play="playLevel"></level-selection>
     <game v-if="gameC" @toMenu="menuC = true; gameC = false" :initialLevel="level"></game>
   </div>
 </template>
 
 <script>
     import Menuu from './components/Menu.vue';
+    import LevelSelection from './components/LevelSelection.vue';
     import Game from './components/play/Game.vue';
 
     export default {
         name: 'app',
-        components: {Menuu, Game},
+        components: {Menuu, Game, LevelSelection},
         data() {
             return {
                 menuC: true,
+                levelSelectionC: false,
                 gameC: false,
                 level: 3
             };
@@ -24,17 +27,17 @@
                 this.menuC = false;
                 this.gameC = true;
             },
+            playLevel(level) {
+                this.level = level;
+                this.levelSelectionC = false;
+                this.gameC = true;
+            },
         },
     };
 </script>
 
 <style>
   #app {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
+    font-family: Verdana, Geneva, sans-serif;
   }
 </style>
---
+++ src/components/LevelSelection.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="level-selection">
+        <div class="levels-container">
+            <div class="level" v-for="level in levels" :key="level.index" @click="$emit('play', level.number)">
+                {{level.number}}
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import {levels} from './data/levels';
+
+    export default {
+        name: 'level-selection',
+        props: [],
+        data() {
+            return {
+                levels: levels
+            };
+        },
+        methods: {},
+        beforeMount() {
+        },
+    };
+</script>
+
+<style scoped>
+
+    .levels-container {
+        display: flex;
+        flex-wrap: wrap;
+        padding: 10px;
+    }
+
+    .level {
+        border: 1px solid black;
+        border-radius: 5px;
+        width: 100px;
+        height: 100px;
+        text-align: center;
+        font-weight: bold;
+        font-size: 30px;
+        line-height: 100px;
+        margin: 10px;
+    }
+
+    @media screen and (max-width: 550px) {
+    }
+</style>
